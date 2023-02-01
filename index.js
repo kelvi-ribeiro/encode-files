09ad2cb5eeb6b5dc2dc8e0d65b853efc4c2a40a8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2023-02-01 11:06:08 -0300

    Finishing first functional version

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+require('dotenv').config();
 const Path = require("path");
 const FS = require("fs");
 let files = [];
@@ -15,14 +16,47 @@ function ThroughDirectory(Directory) {
     });
 }
 
-ThroughDirectory("..");
+function getFileContent(filename) {
+    const buffer = FS.readFileSync(filename);
+    return buffer.toString();
+}
 
-files.filter(f => !f.includes("bin") && !f.includes("obj")).forEach(async file => {
-    const buffer = FS.readFileSync(file);
-    console.log(file)
-    const fileContent = buffer.toString();
-    console.log(fileContent);
-    FS.appendFileSync("./test.txt", fileContent + "\n" + file + "\n", () => {})
+function getContentToSave(fileContent) {
+    if (process.env.ENCODE_OR_DECODE = "ENCODE") {
+        return Buffer.from(fileContent).toString('base64');
+    } else {
+        return Buffer.from(fileContent, 'base64').toString('ascii');
+    }
+}
+
+function getCompleteFilePath(file) {
+    return process.env.FOLTER_PATH_TO_PASTE + "\\" + file.replace(process.env.FOLDER_PATH_BASE_TO_IGNORE_ON_PASTE, "")
+}
+
+function getFilename(fullFilename) {
+    return fullFilename.substring(fullFilename.lastIndexOf("\\") + 1, fullFilename.length)
+}
+
+function getDiretoryName(filePath) {
+    return filePath.substring(0, filePath.lastIndexOf("\\") + 1)
+}
+
+function createDirectoryIfDoesNotExists(directoryName) {
+    if (!FS.existsSync(directoryName)) {
+        FS.mkdirSync(directoryName, { recursive: true }, () => { })
+    }
+}
+
+ThroughDirectory(process.env.FOLDER_PATH_TO_COPY);
+
+files.filter(f => !f.includes("bin") && !f.includes("obj")).forEach((file, index) => {
+    const fileContent = getFileContent(file)
+    const fileContentToSave = getContentToSave(fileContent);
+    var filePath = getCompleteFilePath(file)
+    const filename = getFilename(filePath)
+    console.log(`Starting to copy the file: '${filename}'`)
+    console.log(`Remaining files: ${index + 1}/${files.length}`)
+    const directoryName = getDiretoryName(filePath)
+    createDirectoryIfDoesNotExists(directoryName)
+    FS.writeFileSync(filePath, fileContentToSave, () => { })
 });
-console.log(Buffer.from("Hello World").toString('base64'));
-console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii'))
