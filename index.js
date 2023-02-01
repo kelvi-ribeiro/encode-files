e288bc9b4a3322c73f31a7246975c8778050616e
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2023-02-01 12:55:57 -0300

    Refactoring creating a file for all functions

--- index.js
+++ index.js
@@ -1,62 +1,10 @@
 
-require('dotenv').config();
-const Path = require("path");
-const FS = require("fs");
-let files = [];
+const lib = require("./lib");
 
-function ThroughDirectory(Directory) {
-    FS.readdirSync(Directory).forEach(file => {
-        const Absolute = Path.join(Directory, file);
-        if (FS.statSync(Absolute).isDirectory()) {
-            return ThroughDirectory(Absolute);
-        }
-        else {
-            return files.push(Absolute);
-        }
-    });
+
+
+function main(){
+    lib.copyAllFiles();
 }
 
-function getFileContent(filename) {
-    const buffer = FS.readFileSync(filename);
-    return buffer.toString();
-}
-
-function getContentToSave(fileContent) {
-    if (process.env.ENCODE_OR_DECODE = "ENCODE") {
-        return Buffer.from(fileContent).toString('base64');
-    } else {
-        return Buffer.from(fileContent, 'base64').toString('ascii');
-    }
-}
-
-function getCompleteFilePath(file) {
-    return process.env.FOLTER_PATH_TO_PASTE + "\\" + file.replace(process.env.FOLDER_PATH_BASE_TO_IGNORE_ON_PASTE, "")
-}
-
-function getFilename(fullFilename) {
-    return fullFilename.substring(fullFilename.lastIndexOf("\\") + 1, fullFilename.length)
-}
-
-function getDiretoryName(filePath) {
-    return filePath.substring(0, filePath.lastIndexOf("\\") + 1)
-}
-
-function createDirectoryIfDoesNotExists(directoryName) {
-    if (!FS.existsSync(directoryName)) {
-        FS.mkdirSync(directoryName, { recursive: true }, () => { })
-    }
-}
-
-ThroughDirectory(process.env.FOLDER_PATH_TO_COPY);
-
-files.filter(f => !f.includes("bin") && !f.includes("obj")).forEach((file, index) => {
-    const fileContent = getFileContent(file)
-    const fileContentToSave = getContentToSave(fileContent);
-    var filePath = getCompleteFilePath(file)
-    const filename = getFilename(filePath)
-    console.log(`Starting to copy the file: '${filename}'`)
-    console.log(`Remaining files: ${index + 1}/${files.length}`)
-    const directoryName = getDiretoryName(filePath)
-    createDirectoryIfDoesNotExists(directoryName)
-    FS.writeFileSync(filePath, fileContentToSave, () => { })
-});
+main()
---
+++ lib.js
@@ -0,0 +1,72 @@
+const { promises: fs } = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+
+async function throughDirectory(directory, files) {
+    const items = await fs.readdir(directory);
+    for (const item of items) {
+        const absolute = path.join(directory, item);
+        const stat = await fs.stat(absolute);
+        if (stat.isDirectory()) {
+            await throughDirectory(absolute, files);
+        } else {
+            files.push(absolute);
+        }
+    }
+}
+
+async function getFileContent(filename) {
+    return (await fs.readFile(filename)).toString();
+}
+
+async function getContentToSave(fileContent) {
+    return process.env.ENCODE_OR_DECODE === "ENCODE"
+        ? Buffer.from(fileContent).toString("base64")
+        : Buffer.from(fileContent, "base64").toString("ascii");
+}
+
+function getCompleteFilePath(file) {
+    return path.join(process.env.FOLTER_PATH_TO_PASTE, file.replace(process.env.FOLDER_PATH_BASE_TO_IGNORE_ON_PASTE, ""));
+}
+
+async function createDirectoryIfDoesNotExists(directoryName) {
+    try {
+        await fs.mkdir(directoryName, { recursive: true });
+    } catch (error) {
+        if (error.code !== "EEXIST") {
+            throw error;
+        }
+    }
+}
+
+
+function removeAllIgnoredFiles(files) {
+    console.log("files or folder to ignore: " + process.env.IGNORED_FILES_OR_FOLDERS);
+    var ignoredFolders = process.env.IGNORED_FILES_OR_FOLDERS.split(',');
+    return files.filter(f => !ignoredFolders.some(i => f.includes(i)));
+}
+
+async function copyAllFiles() {
+    const allFiles = [];
+    await throughDirectory(process.env.FOLDER_PATH_TO_COPY, allFiles);
+    console.log(`${allFiles.length} were found in total`);
+    const files = removeAllIgnoredFiles(allFiles);
+    console.log(`${files.length} were found to copy`);
+    for (let i = 0; i < files.length; i++) {
+        console.log(`Processing: ${i + 1}/${files.length}`);
+        const file = files[i];
+        const fileContent = await getFileContent(file);
+        const fileContentToSave = await getContentToSave(fileContent);
+        const filePath = getCompleteFilePath(file);
+        const filename = path.basename(filePath);
+        console.log(`Starting to copy the file: '${filename}'`);
+        const directoryName = path.dirname(filePath)
+        await createDirectoryIfDoesNotExists(directoryName);
+        await fs.writeFile(filePath, fileContentToSave);
+    }
+}
+
+module.exports = {
+    copyAllFiles
+};
